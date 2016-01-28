ecd6c71dc54df5fff993f3debe038a4ce74798a6
odatti <shoichi.oda@gmail.com>
2016-01-29 06:56:08 +0900

    重みを考慮して戦うちょっと間抜けなAIの追加

--- osero/main.c
+++ osero/main.c
@@ -61,7 +61,7 @@ void game_menu();
 void nextMenu();
 
 /** プレイする人の情報 */
-#define PLAYER_TYPE_MAX 2
+#define PLAYER_TYPE_MAX 3
 enum{
 	PLAYER = 0,
 	EASY = 1,
@@ -237,28 +237,42 @@ static volatile int ai_normal_map[LED_SIZE][LED_SIZE] = {
 	{-12,-15, -3, -3, -3, -3,-15,-12},
 	{ 30,-12,  0, -1, -1,  0,-12, 30}
 };
+typedef struct AI_TARGET{
+	int index;// 座標
+	int score;// 重み
+}AI_TARGET;
 void normal_ai(int turn){
 	int x,y;
-	int putList[64];
+	int i;
+	AI_TARGET putList[64];
 	int index = 0;
-	for(x = 0;x < 64;x++){
-		putList[64] = 0;
+	int decide = 0;
+	for(i = 0;i < 64;i++){
+		putList[i].index = 0;
+		putList[i].score = 0;
 	}
 	for(y = 0;y < LED_SIZE;y++){
 		for(x = 0;x < LED_SIZE;x++){
 			if(judgePutStone(x,y,turn) == 1){
-				putList[index] = y*LED_SIZE+x;
+				putList[index].index = y*LED_SIZE+x;
+				putList[index].score = ai_normal_map[y][x];
 				index++;
 			}
 		}
 	}
-	if(index==0)
-		return;
+	// 一番スコアの高い場所を調べる(リニアサーチなので予測しやすい)
+	for(i=0;i<index;i++){
+		if(putList[decide].score < putList[i].score){
+			decide = i;
+		}
+	}
 	
-	index = putList[random_rand() % index];	
-	x = index % LED_SIZE;
-	y = (int)(index / LED_SIZE);
-	putStone(x, y, turn);
+	if(index != 0){
+		index = putList[decide].index;	
+		x = index % LED_SIZE;
+		y = (int)(index / LED_SIZE);
+		putStone(x, y, turn);
+	}
 	if(isFinishGame(target_getTurn()) > 0) gameState = FINISH;
 }
 
